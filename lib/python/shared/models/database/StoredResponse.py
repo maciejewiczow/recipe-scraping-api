f1bf16ff00c4450a05ea175101ed9a41606e7963
Mafciejewicz <melonik0.9846@gmail.com>
2025-12-20 17:09:19 +0100

    Change projection inheritance

--- lib/python/shared/models/database/StoredResponse.py
+++ lib/python/shared/models/database/StoredResponse.py
@@ -1,15 +1,21 @@
 from typing import Annotated
+
+from pydantic import Field
 from shared.models.DTO.ProcessIngredientsInput import (
     IngredientToProcessWithLangInfoDTO,
 )
-from shared.utils.dynamodb import DynamodbModel, PrimaryKey
+from shared.utils.dynamodb import DynamodbModel, PrimaryKey, TTLField
 
 
-class StoredResponseProjection(DynamodbModel):
-    TaskToken: str
+class StoredResponse(DynamodbModel):
     ResponseId: Annotated[str, PrimaryKey(key_type="hash")]
-
-
-class StoredResponse(StoredResponseProjection):
+    TaskToken: str
+    ExpiresIn: TTLField
     OriginalIngredientInput: IngredientToProcessWithLangInfoDTO
     RetryCount: int
+
+
+class StoredResponseProjection(StoredResponse):
+    ExpiresIn: TTLField = Field(exclude=True)
+    OriginalIngredientInput: IngredientToProcessWithLangInfoDTO = Field(exclude=True)
+    RetryCount: int = Field(exclude=True)
